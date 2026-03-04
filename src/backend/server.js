255cfb4e4aeb0f876b566e1b3d51b71b8c65cf14
marikannan-developer <marim36079@gmail.com>
2026-03-04 12:26:12 +0530

    fixed port for render deploy

--- src/backend/server.js
+++ src/backend/server.js
@@ -147,8 +147,10 @@ app.get("/", (req, res) => {
 // ============================
 // SERVER START
 // ============================
-app.listen(5000, () => {
-  console.log("🚀 Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+ console.log("🚀 Server running on port " + PORT);
 });
 
 
